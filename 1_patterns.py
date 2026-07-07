7c859e7ba1ea648daf0df2fc17676f10ca511d21
OMPRAKASH DHANAK <ronakdhanak795@gmail.com>
2026-07-07 19:32:46 +0530

    Square pattern using for loop

---
+++ 1_patterns.py
@@ -0,0 +1,7 @@
+#  Squers using for loop
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+    # Outer Loop = No.rows
+    # Inner Loop = how many items are printed in each row
